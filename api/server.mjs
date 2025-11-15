c8a49f2c6631e900f3324f7d9d9d524e0f62b24b
Paras Dev <parasbhatiwal@gmail.com>
2025-11-15 16:29:51 +0530

    fixed deployment to vercel

---
+++ api/server.mjs
@@ -0,0 +1,6 @@
+// Import the request handler from the built server
+import { reqHandler } from '../dist/angular-ssr/server/server.mjs';
+
+// Export as default handler for Vercel
+export default reqHandler;
+
