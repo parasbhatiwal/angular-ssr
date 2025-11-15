6c49f91cb1946081317a31e9fcfdeeae97fed732
Paras Dev <parasbhatiwal@gmail.com>
2025-11-15 17:19:11 +0530

    added error log

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
-import '../dist/angular-ssr/server/server.mjs';
-
 export default async function handler(req, res) {
-  const { default: server } = await import('../dist/angular-ssr/server/server.mjs');
-  return server(req, res);
+  try {
+    // Dynamically import the Angular server
+    const { default: server } = await import('../dist/angular-ssr/server/server.mjs');
+    
+    // The server export is typically an Express app
+    server.handle(req, res);
+  } catch (error) {
+    console.error('Server error:', error);
+    res.status(500).send('Internal Server Error');
+  }
 }
